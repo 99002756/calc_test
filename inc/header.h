a5ecafb226510f9ffdf1c37ae63dba33e8bcd3f4
Training <Binduja.S@ltts.com>
2020-10-10 10:45:10 +0530

    f

---
+++ inc/header.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+ 
+#define KEY "Enter the calculator Operation you want to do:"
+ 
+
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+void calculator_operations();
